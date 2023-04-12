c08f45087230920729556c1e47e4cb2fcc6384cb
kcarnrite <59613980+kcarnrite@users.noreply.github.com>
2023-04-12 15:22:42 -0400

    Added local functionality to mark item as checked / undo)

--- todoapp/src/common/components/toDoItem.js
+++ todoapp/src/common/components/toDoItem.js
@@ -1,17 +1,16 @@
 
 //TODO: Need to display ToDoItem and not just placeholder
-export default function ToDoItem({title, isChecked }) {
+export default function ToDoItem({ item, handleCheckClick }) {
     function handleCheck() {
         console.log(title + " checked? " + isChecked)
     }
     return (
         <div className="listItemContainer"> 
-            <button onClick={handleCheck}>Complete</button>
-            {isChecked ? 
-                <s><p className="itemTitle"> {title}</p></s> :
-                <p className="itemTitle"> {title}</p>
+            <button onClick={() => handleCheckClick(item.id)}>Complete</button>
+            {item.isChecked ? 
+                <s><p className="itemTitle"> {item.title}</p></s> :
+                <p className="itemTitle"> {item.title}</p>
             }
-
         </div>
     )
 }
--- todoapp/src/pages/list/[id].js
+++ todoapp/src/pages/list/[id].js
@@ -8,13 +8,28 @@ export default function List() {
     const router = useRouter();
     const {id} = router.query;
     const[todoItems, setTodoItems] = useState(getListItems(id));
+
+    function updateChecked(id) {
+        const newArr = todoItems.map(item => {
+            if(item.id === id) {
+                return {...item, isChecked: !item.isChecked};
+            } 
+            else {
+                return item;
+            }
+        })
+        setTodoItems(newArr);
+    }
     
 
     return (
         <ListContainer>
             {todoItems.map(item => {
                 return (
-                    <ToDoItem title={item.title} key={item.id} isChecked={item.isChecked}/>
+                    <ToDoItem 
+                        item = {item}
+                        handleCheckClick={id => updateChecked(id)}
+                     />
                 )
             })}
         </ListContainer>
